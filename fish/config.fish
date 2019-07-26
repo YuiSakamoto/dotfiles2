61dcc7bde0f64a2b42d7ec30207d6f017f83bd85
Yui Sakamoto <yui.k0303@gmail.com>
2019-07-26 21:24:03 +0900

    Added eval for hub.

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,6 @@
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/Users/yui_tang/google-cloud-sdk/path.fish.inc' ]; . '/Users/yui_tang/google-cloud-sdk/path.fish.inc'; end
+
+# set up alias for hub as git
+eval (hub alias -s)
