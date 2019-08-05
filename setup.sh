42c146880e1dc9ebdccb416dca4f7c44bd043955
Yui Sakamoto <yui.k0303@gmail.com>
2019-08-05 19:36:11 +0900

    Added a alias and update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,12 +4,12 @@ DOT_FILES=( .gitconfig .gitignore .tmux.conf)
 
 for file in ${DOT_FILES[@]}
 do
-    ln -s $HOME/dotfiles2/$file $HOME/$file
+    ln -s $(cd $(dirname $0);pwd)/$file $HOME/$file
 done
 
 DOT_FILES_TO_CONFIG_DIR=( fish nvim karabiner)
 
 for file in ${DOT_FILES_TO_CONFIG_DIR[@]}
 do
-    ln -s $HOME/dotfiles2/$file $HOME/.config/$file
+    ln -s $(cd $(dirname $0);pwd)/$file $HOME/.config/$file
 done
