d7bc0f346d88ea438bff4ab0442181641935d503
Yui Sakamoto <yui.k0303@gmail.com>
2020-04-03 22:15:00 +0900

    Added some paths.

--- fish/conf.d/path.fish
+++ fish/conf.d/path.fish
@@ -2,6 +2,10 @@ set -x GOPATH $HOME
 set -x GOBIN $HOME/bin
 set -x PATH $PATH $GOPATH/bin
 
+set -gx PKG_CONFIG_PATH "/usr/local/opt/krb5/lib/pkgconfig" $PKG_CONFIG_PATH
+set -gx PKG_CONFIG_PATH "/usr/local/opt/openssl@1.1/lib/pkgconfig" $PKG_CONFIG_PATH
+set -gx PKG_CONFIG_PATH "/usr/local/opt/zlib/lib/pkgconfig" $PKG_CONFIG_PATH
+
 set -x PATH $HOME/.anyenv/bin $PATH
 eval (anyenv init - | source)
 
--- fish/config.fish
+++ fish/config.fish
@@ -3,3 +3,4 @@ if [ -f '/Users/yui_tang/google-cloud-sdk/path.fish.inc' ]; . '/Users/yui_tang/g
 
 # set up alias for hub as git
 eval (hub alias -s)
+set -g fish_user_paths "/usr/local/opt/bzip2/bin" $fish_user_paths
