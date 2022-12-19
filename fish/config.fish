bb53e6ab9bb0401118f63dc4c90987a2df4122ed
Yui Sakamoto <yuitang.dev@gmail.com>
2022-12-19 15:31:35 +0900

    Update

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,11 @@
 eval (hub alias -s)
 set -g fish_user_paths "/usr/local/opt/bzip2/bin" $fish_user_paths
 
+eval (/opt/homebrew/bin/brew shellenv)
+
+set -x PATH $HOME/.anyenv/bin $PATH
+eval (anyenv init - | source)
+
 eval (ssh-agent -c)
-ssh-add ~/.ssh/id_rsa
-ssh-add ~/.ssh/stash_rsa
+ssh-add ~/.ssh/id_ed25519
 tm
