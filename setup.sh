d5f1100d2b115f02fe71818bf2021783a7a85a18
Yui Sakamoto <yuitang.dev@gmail.com>
2022-12-19 15:43:22 +0900

    Add setup script for bin

--- setup.sh
+++ setup.sh
@@ -1,19 +1,33 @@
 #!/bin/bash
 
-DOT_FILES=( .gitconfig .gitignore .tmux.conf)
+DOT_FILES=(.gitconfig .gitignore .tmux.conf)
 
-for file in ${DOT_FILES[@]}
-do
-    ln -s $(cd $(dirname $0);pwd)/$file $HOME/$file
+for file in ${DOT_FILES[@]}; do
+  ln -s $(
+    cd $(dirname $0) || exit
+    pwd
+  )/$file $HOME/$file
 done
 
-DOT_FILES_TO_CONFIG_DIR=( fish nvim karabiner)
+DOT_FILES_TO_CONFIG_DIR=(fish nvim karabiner)
 
 if [[ ! -d ~/.config ]]; then
   mkdir ~/.config
 fi
 
-for file in ${DOT_FILES_TO_CONFIG_DIR[@]}
-do
-    ln -s $(cd $(dirname $0);pwd)/$file $HOME/.config/$file
+for file in ${DOT_FILES_TO_CONFIG_DIR[@]}; do
+  ln -s $(
+    cd $(dirname $0) || exit
+    pwd
+  )/$file $HOME/.config/$file
+done
+
+BIN_FILES=(ssm)
+
+for file in ${BIN_FILES[@]}; do
+  sudo ln -s $(
+    cd $(dirname $0) || exit
+    pwd
+  )/bin/$file /usr/local/bin/$file
+  chmod 755 /usr/local/bin/$file
 done
