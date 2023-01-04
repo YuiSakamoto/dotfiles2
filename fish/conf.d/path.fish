c26ac4d5cab2cfecb20af765b84712b0620cb0ae
Yui Sakamoto <yuitang.dev@gmail.com>
2023-01-04 10:43:00 +0900

    Happy new year!!

--- fish/conf.d/path.fish
+++ fish/conf.d/path.fish
@@ -9,10 +9,10 @@ set -gx PKG_CONFIG_PATH "/usr/local/opt/zlib/lib/pkgconfig" $PKG_CONFIG_PATH
 set -x PATH $HOME/.anyenv/bin $PATH
 eval (anyenv init - | source)
 
-set -x GOENV_ROOT $HOME/.goenv
-set -x PATH $GOENV_ROOT/bin:$PATH
-set -x PATH $HOME/.goenv/bin:$PATH
-eval (goenv init -)
+# set -x GOENV_ROOT $HOME/.goenv
+# set -x PATH $GOENV_ROOT/bin:$PATH
+# set -x PATH $HOME/.goenv/bin:$PATH
+# eval (goenv init -)
 
 set -x PATH $PATH:/usr/local/bin:$HOME/.composer/vendor/bin
 set -x PATH $PATH:/usr/local/bin/terraform
